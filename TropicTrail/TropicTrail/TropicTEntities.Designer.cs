1cd8b9ac3e658641f5e72cd219ddca4eaae31246
Jamiss <113078836+jimsjamiss@users.noreply.github.com>
2024-04-17 20:20:06 +0800

    asd

--- TropicTrail/TropicTrail/TropicTEntities.Designer.cs
+++ TropicTrail/TropicTrail/TropicTEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\dosad\Desktop\TropicT\TropicTrail\TropicTrail\ModelTropicT.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\embud\OneDrive\Desktop\TropicT-James\TropicTrail\TropicTrail\TropicTEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
